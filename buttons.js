0170f763c9f50de2808381c41459a3254b9b4520
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-06-28 16:36:42 -0400

    more

--- buttons.js
+++ buttons.js
@@ -1,6 +1,19 @@
 // Save it to a variable called `topics`
 
-var topics = ["SATC", "Britney Spears", "princess and the frog","Drake", "clueless", "Beyonce", "Sonja Morgan", "corgi", "leonardo dicaprio", "trisha paytas"];
+var topics = ["SATC", "Britney Spears", "princess and the frog","Drake", "clueless", "Beyonce", "Sonja Morgan", "corgi", "leonardo dicaprio", "trisha paytas", "Serena Williams"];
+
+
+
+for (var j = 0; j < topics.length; j++) {
+    // $("#buttons").empty();
+
+    var buttontag = $("<button>");
+
+    buttontag.html(topics[j]).attr("data-topics", topics[j]).addClass("button");
+    
+    $("#buttons").append(buttontag);
+
+}
 
 
 // $(document).ready(function() {
@@ -8,22 +21,49 @@ var topics = ["SATC", "Britney Spears", "princess and the frog","Drake", "cluele
 // });
 
 // Event listener for all button elements
-$("button").on("click", function() {
-    var topics = $(this).attr("data-topics");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    topics + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+$(document).ready(function(){
+$(".button").on("click", function() {
+    var query = $(this).attr("data-topics");
+    console.log(query);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    query + "&api_key=dc6zaTOxFJmzC&limit=10";
+    
 
 // Performing our AJAX GET request
 $.ajax({
     url: queryURL,
     method: "GET"
 })
+
+
 .then(function(response) {
-    var results = response.data;
+console.log(response);
+
+var results = response.data;
+for (var i = 0; i < results.length; i++) {
+var image = results[i].images.fixed_width.url;
+
+console.log(image);
+
+var divtag = $("<div>");
+
+var imgtag = $("<img>");
+
+imgtag.attr("src", image).attr("width", "250");
+
+divtag.append(imgtag);
+
+$("#pics").append(divtag);
+
+
+
+
+}
 
 })
 
 // $("gifs-here").append(gifDiv);
 
+});
 });
